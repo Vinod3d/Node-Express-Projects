dcbdcca062ea943397d3faa8db8820dd5cd58d90
Vinod Chandra <vinodchandra979@gmail.com>
2024-01-21 22:21:02 +0530

    sending email not working

--- 4-JWT-gekky/controllers/userController.js
+++ 4-JWT-gekky/controllers/userController.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/User')
 const bcrypt = require('bcrypt')
 const JWT = require('jsonwebtoken')
+const { transporter } = require('../config/emailConfig')
 
 class UserController {
     static userRegistration = async (req, res)=> {
@@ -86,6 +87,91 @@ class UserController {
 
     static loggedUser = async (req, res)=>{
         res.send({"user" : req.user})
+    }
+
+
+    static sendUserPasswordResetEmail = async (req, res) => {
+        try {
+            const { email } = req.body;
+
+            if (!email) {
+                return res.send({ "status": "failed", "message": "Email Field is required!" });
+            }
+
+            const user = await UserModel.findOne({ email });
+
+            if (user) {
+                const secret = user._id + process.env.JWT_SECRET_KEY;
+                const token = JWT.sign({ userID: user._id }, secret, { expiresIn: '15m' });
+                const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`;
+                console.log(link);
+
+                     // Send Email
+                    // const info = await transporter.sendMail({
+                    //     from: process.env.EMAIL_FROM,
+                    //     to: email,
+                    //     subject: 'Your Password Reset Link',
+                    //     html: `<a href=${link}>Click Here</a> to Reset Your Password`
+                    // });
+
+                    const mailoptions = {
+                        from: process.env.EMAIL_FROM,
+                        to: email,
+                        subject: 'portfolio',
+                        text: 'Your Password Reset Link',
+                        html: `<a href=${link}>Click Here</a> to Reset Your Password`
+                    }
+                    
+                    transporter.sendMail(mailoptions, (err, result) => {
+                        if (err){
+                        console.log(err)
+                            res.json('opps error occured')
+                        } else{
+                            res.json('thanks for e-mailing me');
+                        }
+                    })
+                
+                
+               
+                res.send({
+                    "status": "success",
+                    "message": "Password Reset Email Sent... Please Check Your Email",
+                    "info": info
+                });
+            } else {
+                res.send({ "status": "failed", "message": "Email doesn't exist" });
+            }
+        } catch (error) {
+            console.error('Error sending password reset email:', error);
+            res.status(500).send({ "status": "failed", "message": "Internal Server Error" });
+        }
+    }
+
+
+    static userPasswordReset = async(req, res)=>{
+        const {password, password_confirmation} = req.body
+        const {id, token} = req.params
+        const user = await UserModel.findById(id)
+        const new_secret = user._id + process.env.JWT_SECRET_KEY
+        try {
+            JWT.verify(token, new_secret)
+            if(password && password_confirmation){
+                if(password === password_confirmation){
+                    if(password !== password_confirmation){
+                        res.send({"status":"failed", "message":"New Password and Confirm New Password doesn't match"})
+                    } else{
+                        const salt = await bcrypt.genSalt(10)
+                        const newHashedPassword = await bcrypt.hash(password, salt)
+                        await UserModel.findByIdAndUpdate(user._id, {$set : {password: newHashedPassword}})
+                        res.send({"status" : "success", "message" : "Password Reset succesfully"})
+                    }
+                }
+            } else{
+                return res.send({"status":"failed", "message":"Both fields are required!"})
+            }
+        } catch (error) {
+            res.send({"status" : "failed", "message": "Invalid Token"})
+        }
     }
 }
 
