2251e7ff22f53d2f37ef0b53f120090e87de0300
Vinod Chandra <vinodchandra979@gmail.com>
2024-02-17 21:10:06 +0530

    E-commerce-

--- 8-E-Commerce-API/app.js
+++ 8-E-Commerce-API/app.js
@@ -1,1 +1,68 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors');
+
+// express
+const express = require('express');
+const app = express();
+
+// rest of the packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
+// database
+const connectDB = require('./db/connect');
+
+
+//  routers
+const authRouter = require('./routes/authRoutes');
+
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('e-commerce api');
+});
+
+app.use('/api/v1/auth', authRouter);
+
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- 8-E-Commerce-API/db/connect.js
+++ 8-E-Commerce-API/db/connect.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+    .then(() => console.log("CONNECTED TO THE DB..."))
+    .catch((err) => console.log(err));
 };
 
-module.exports = connectDB;
+
+module.exports = connectDB
